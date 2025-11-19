8f5c47722bc99ba6f0901ebf0392443269372c8e
Dilnaz <153282013+Dilnazochka@users.noreply.github.com>
2025-11-19 16:28:00 +0600

    first commit

---
+++ src/main/java/kg/alatoo/myhealt/MyHealtApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.myhealt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHealtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/alatoo/myhealt/MyHealtApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.myhealt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHealtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
